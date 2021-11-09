261bf2ff58b01d91c604d0fab241ed995f5cd325
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-11-08 22:23:55 -0300

    ajuste de pasta

---
+++ front/src/api/api.js
@@ -0,0 +1,49 @@
+export const Api = {
+  baseUrl: "http://localhost:3001",
+
+  createUrl: () => Api.baseUrl + "/personagens/create/",
+
+  readAllUrl: () => Api.baseUrl + "/personagens/readAll/",
+  readSingleUrl: (id) => Api.baseUrl + "/personagens/readSingle/" + id,
+
+  updateUrl: (id) => Api.baseUrl + "/personagens/update/" + id,
+
+  deleteUrl: (id) => Api.baseUrl + "/personagens/delete/" + id,
+  deleteAllUrl: () => Api.baseUrl + "/personagens/create",
+
+  // Create
+  buildApiPostRequest: (url, body) => {
+    return fetch(url, {
+      method: "POST",
+      headers: new Headers({
+        "Content-type": "application/json",
+      }),
+      body: JSON.stringify(body),
+    });
+  },
+
+  // ReadAll
+  buildApiGetRequest: (url) => {
+    return fetch(url, {
+      method: "GET",
+    });
+  },
+
+  // UpdateById
+  buildApiPutRequest: (url, body) => {
+    return fetch(url, {
+      method: "PUT",
+      headers: new Headers({
+        "Content-type": "application/json",
+      }),
+      body: JSON.stringify(body),
+    });
+  },
+
+  // DeleteAll
+  buildApiDeleteRequest: (url) => {
+    return fetch(url, {
+      method: "DELETE",
+    });
+  },
+};
